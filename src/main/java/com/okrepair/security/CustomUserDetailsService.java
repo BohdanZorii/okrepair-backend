16c23ec67cc3bdc378ab571d68485ffb97ea6d97
Zorii Bohdan <bodkazorii@gmail.com>
2024-09-08 20:53:43 +0300

    add CustomUserDetailsService

---
+++ src/main/java/com/okrepair/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.okrepair.security;
+
+import com.okrepair.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() ->
+                new EntityNotFoundException("Can't find user by email " + username));
+    }
+}
