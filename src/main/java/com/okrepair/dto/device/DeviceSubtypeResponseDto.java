9af16aed5f96e31a0fb278299165f5c735f46b88
Zorii Bohdan <bodkazorii@gmail.com>
2024-09-19 12:47:53 +0300

    add device subtype model

---
+++ src/main/java/com/okrepair/dto/device/DeviceSubtypeResponseDto.java
@@ -0,0 +1,8 @@
+package com.okrepair.dto.device;
+
+public record DeviceSubtypeResponseDto(
+        Long id,
+        String name,
+        String imageUrl
+) {
+}
--- src/main/java/com/okrepair/model/Device.java
+++ src/main/java/com/okrepair/model/Device.java
@@ -1,11 +1,18 @@
 package com.okrepair.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,4 +28,9 @@ public class Device {
     private String name;
     private String imageUrl;
     private String description;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(name = "devices_subtypes",
+            joinColumns = @JoinColumn(name = "device_id"),
+            inverseJoinColumns = @JoinColumn(name = "device_subtype_id"))
+    private List<DeviceSubtype> deviceSubtypes = new ArrayList<>();
 }
